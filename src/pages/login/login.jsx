e175907f8d6b3e74d88e45775de9dda39b17cb75
1371701858@qq.com <ch200810>
2021-01-18 15:50:07 +0800

    1.V1.0.0 修改入口函数

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import {Form}  from 'antd'
 
-function Login(props) {
+function Login(){
     return (
         <div>
-
+            <h2>你好</h2>
         </div>
     );
 }
